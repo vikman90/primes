eb92fb8360fa63614fdd16a60b5f20a95350b79c
Marta Gómez Macías <marta@wazuh.com>
2017-10-12 20:27:38 +0200

    Add argparse

--- benchmark.py
+++ benchmark.py
@@ -4,16 +4,26 @@ from subprocess import check_output
 import pandas as pd
 import matplotlib.pyplot as plt
 from random import choice
+import argparse
 
-begin = 1000000
-end =   50000000
+parser = argparse.ArgumentParser(usage="%(prog)s [options]",
+                    description='Ejecuta un benchmark con distintas versiones del algoritmo.')
+parser.add_argument('--begin', dest='begin', type=int, default=1000000,
+                    help="Número desde el que empezar el test")
+parser.add_argument('--end', dest='end', type=int, default=50000000,
+                    help="Número desde el que acabar el test")
+parser.add_argument('--step', dest='step', type=int, default=0,
+                    help="Número por el que incrementar el test")
+parser.add_argument('--style', dest='style', type=str, default=choice(plt.style.available),
+                    help="Estilo de la gráfica final")
 
 def gettime(args):
     out = str(check_output(args, shell=True))
     return float(out.split("Tiempo: ")[1].split()[0])
 
-def get_dataframe():
-    data_series = list(range(begin, end + 1, begin))
+def get_dataframe(begin, end, step):
+    perf_step = step if step != 0 else begin
+    data_series = list(range(begin, end + 1, perf_step))
     time_data = pd.DataFrame(columns=['Criba','Destructivo Paralelo', 'Destructivo Secuencial'],
                              index=data_series)
 
@@ -28,15 +38,15 @@ def get_dataframe():
 
     return time_data
 
-def plot(time_data):
+def plot(time_data, style):
     plt.figure()
-    style = choice(plt.style.available)
     print("Usando el estilo de gráfica {0}\n".format(style))
     plt.style.use(style)
     time_data.plot()
     plt.savefig('time_compare.png')
 
 if __name__ == "__main__":
-    time_data = get_dataframe()
-    plot(time_data)
+    ns = parser.parse_args()
+    time_data = get_dataframe(ns.begin, ns.end, ns.step)
+    plot(time_data, ns.style)
     print(time_data)
