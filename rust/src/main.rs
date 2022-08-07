52da0f5833dfb4526b0e6e074dc2548b6d702a6a
Vikman Fernandez-Castro <victor@wazuh.com>
2022-08-07 10:04:15 +0200

    Rust: 2. Discard values but do not remove.
    Runtime for sieving up to 1.000.000: ~190 ms.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -11,42 +11,38 @@ const DEFAULT_SIZE: u32 = 1000000;
 
 fn main() {
     let size = parse_args();
-    let mut primes: Vec<u32> = (2..=size).collect();
+    let mut primes: Vec<Option<u32>> = (2..=size).map(|s| Option::Some(s)).collect();
 
     let now = Instant::now();
     sieve(&mut primes);
     let duration = now.elapsed();
-    primes.sort();
 
     for i in primes {
-        println!("{}", i);
+        if let Some(pi) = i {
+            println!("{}", pi);
+        }
     }
 
     eprintln!("Time: {:.6} ms.", duration.as_nanos() as f64 / 1e6);
 }
 
-fn sieve(primes: &mut Vec<u32>) {
-    // Bug: leaks some values due to swapping and j=i+1.
+fn sieve(primes: &mut Vec<Option<u32>>) {
+    let bound = (primes.last().unwrap().unwrap() as f64).sqrt() as u32;
 
-    let bound = (*primes.last().unwrap() as f64).sqrt() as u32;
+    for i in 0..primes.len() {
+        if let Some(pi) = primes[i] {
+            if pi > bound {
+                break;
+            }
 
-    let mut i = 0;
-    while i < primes.len() {
-        if primes[i] > bound {
-            i += 1;
-            continue;
-        }
-
-        let mut j = i + 1;
-        while j < primes.len() {
-            if primes[j] % primes[i] == 0 {
-                primes.swap_remove(j);
-            } else {
-                j += 1;
+            for j in i+1..primes.len() {
+                if let Some(pj) = primes[j] {
+                    if pj % pi == 0 {
+                        primes[j] = Option::None;
+                    }
+                }
             }
         }
-
-        i += 1;
     }
 }
 
